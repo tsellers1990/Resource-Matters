a624e6b4b74138ddf6d0ff4e796ea5fad0ced1d4
Tymothy Sellers <tsellers1990@gmail.com>
2020-09-01 14:11:52 -0600

    working on building out the navbar. I added text as a placholder while w we get the logo situated

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,1 +1,12 @@
-/////placholder
+import React from 'react';
+import './Navbar.css'
+
+export const Navbar = () => {
+    return (
+        <div className='wrapper'>
+            <div className='inner'>
+                <h1 className='logo-and-left'>This is the Navbar</h1>
+            </div>
+        </div>
+    )
+}
