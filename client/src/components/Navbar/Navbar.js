3abc046a278de38eeda702dc2f158f7e047b6b8a
tsellers1990 <tsellers1990@gmail.com>
2020-09-19 17:44:39 -0600

    re-fixed the navbar styling

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,29 +1,34 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './Navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export const Navbar = () => {
-    return (
-      <>
-        <div className='wrapper'>
-            <div className='inner row'>
-                  <h1 className='logo-and-left col align-self-center '>This is the Navbar</h1>
-            </div>
+  return (
+    <>
+      <div className="wrapper">
+        <div className="inner">
+          <h1 className="header-text">
+            Justice For All
+          </h1>
+          {/* image tag here... maybe look into putting it all together as an image */}
         </div>
-        <ul className= "nav-btns">
-          <li className= "home-btn">
-            <Link to= "/">
+
+        <ul className="nav-btns">
+          <li className="home-btn">
+            <Link to="/">
               <h4>Home</h4>
             </Link>
           </li>
           <li>
-            <Link to= "/videos">
-              <h4 className= "video-btn">Videos</h4>
+            <Link to="/videos">
+              <h4 className="video-btn">Videos</h4>
             </Link>
           </li>
         </ul>
-      </>
-    )
-}
+
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
