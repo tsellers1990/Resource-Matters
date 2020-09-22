993b1ee2bfb109ea8d3ccee82bd715fce4061fc3
tsellers1990 <tsellers1990@gmail.com>
2020-09-21 19:21:14 -0600

    mixed up the TODO list etc

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import Logo from '../../Images/Logo.PNG'
 
 export const Navbar = () => {
   return (
     <>
       <div className="wrapper">
-        <div className="inner">
-          <h1 className="header-text">
-            Justice For All
-          </h1>
-          {/* image tag here... We'll create the logo and words all as one to make it easier*/}
-        </div>
 
-        <ul className="nav-btns">
-          <li className="home-btn">
-            <Link to="/">
-              <h4>Home</h4>
-            </Link>
-          </li>
-          <li>
-            <Link to="/videos">
-              <h4 className="video-btn">Videos</h4>
-            </Link>
-          </li>
-        </ul>
+        <div className="inner">
+          <div className="logo">
+            <img src={Logo} />
+          </div>
+
+          <ul className="nav-btns">
+            <li className="home-btn">
+              <Link to="/">
+                <h4>Home</h4>
+              </Link>
+            </li>
+            <li classNamme="video-btn">
+              <Link to="/videos">
+                <h4>Videos</h4>
+              </Link>
+            </li>
+          </ul>
+        </div>
 
       </div>
     </>
